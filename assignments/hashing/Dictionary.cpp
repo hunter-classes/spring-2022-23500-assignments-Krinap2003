3c3dbddbdd0c34f34713ca39eaac97fe8e286f10
Krina Patel <KRINA.PATEL96@myhunter.cuny.edu>
2022-05-13 18:59:05 -0400

    added funtion heading for Dictionary class

--- assignments/hashing/Dictionary.cpp
+++ assignments/hashing/Dictionary.cpp
@@ -1,2 +1,22 @@
 #include <iostream>
 #include "Dictionary.h"
+
+
+Dictionary::Dictionary(){
+
+}
+
+Dictionary::~Dictionary(){
+
+}
+void Dictionary::insert(Person p){
+
+}
+
+Person Dictionary::retrivePerson(int id){
+
+}
+
+std::string Dictionary::getAllPeopll(){
+
+}
--- assignments/hashing/Dictionary.h
+++ assignments/hashing/Dictionary.h
@@ -1,6 +1,15 @@
 #pragma once
+#include "OList.h"
 
 class Dictionary{
     private:
+        OList *arr[10];
+
     public: 
+        Dictionary();
+        ~Dictionary();
+        void insert(Person p);
+        Person retrivePerson(int id);
+        std::string getAllPeopll();
+
 };
